beb77b9c7f538d058cd444928c510619c9db9da3
thania09 <tania_villalobos_93@hotmail.com>
2013-12-10 19:52:34 -0600

    tarea1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+namespace Persona
+{
+        class Program
+        {
+                public static void Main(string[] args)
+                {
+                        new persona();
+                        
+                        Console.ReadKey(true);
+                }
+        }
+}
---
+++ persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Persona
+{
+        public class persona
+        {
+                public string nombre;
+                public string email;
+                public int edad;
+                
+                public persona()
+                {
+                        
+                                Console.WriteLine("\n¿Cual es tu nombre? \n\r");
+                                nombre = Console.ReadLine();
+                            Console.WriteLine("\n¿Cual es tu Email? \n\r");
+                            email =  Console.ReadLine();
+                            Console.WriteLine("\n¿Cual es tu edad? \n\r");
+                            edad =  int.Parse(Console.ReadLine());
+                            
+                            Console.Clear();
+                            
+                            Console.WriteLine("\n*"+nombre );
+                            Console.WriteLine("\n*"+email );
+                            Console.WriteLine("\n*"+edad );
+                            
+                            
+                }
+        }
+}
